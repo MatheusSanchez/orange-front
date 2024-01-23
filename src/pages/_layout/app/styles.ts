d17667829f903f1a6bd107410c4383b08e6f0fb6
pedrodecf <contato.pedrodecf@gmail.com>
2024-01-23 13:37:22 -0300

    feat: create layouts for app pages and global style

---
+++ src/pages/_layout/app/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const AppLayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
+export const OutletContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  /* width: 80rem; */
+  margin: 0 auto;
+  padding: 1rem 1.88rem;
+`
