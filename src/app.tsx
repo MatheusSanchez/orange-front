3f81d7b534933d925284b5db7d2e452ab50be8dc
LucSosa <l.sosamachado@gmail.com>
2024-01-23 21:50:23 -0300

    Feat: SignUp Mobile + Factory colors and font

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,17 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Routes } from './routes'
 import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/Themes/defaultTheme'
 
 export function App() {
   return (
-    <HelmetProvider>
-      <Helmet titleTemplate="%s - Orange Portfólio" />
-      <Routes />
-      <GlobalStyle />
-    </HelmetProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <HelmetProvider>
+        <Helmet titleTemplate="%s - Orange Portfólio" />
+        <Routes />
+        <GlobalStyle />
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,10 +2,15 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { AppLayout } from '../pages/_layout/app'
 import { MyProjects } from '../pages/MyProjects'
 import { Feed } from '../pages/Feed'
+import { SignUp } from '../pages/SignUp'
 
 export function AppRoutes() {
   return (
     <Routes>
+      <Route>
+        <Route path="/cadastro" element={<SignUp />} />,
+      </Route>
+      ,
       <Route element={<AppLayout />}>
         <Route path="/meus-projetos" element={<MyProjects />} />,
         <Route path="/descobrir" element={<Feed />} />,
