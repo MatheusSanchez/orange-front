75416852e2f3ebc1a140b2174d1909bf87f47470
Matheus Sanchez <matheus_sanchez@usp.br>
2024-01-28 22:50:05 -0300

    Adding env setup to fix deploy :)

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333', // default port on our backend
-  // baseURL: 'http://localhost:3000', // default port on json server
+  baseURL: import.meta.env.VITE_API_URL,
 })
